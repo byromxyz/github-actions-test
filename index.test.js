56540a535fb90366d42791ea36713265b1a8e994
Ash Byrom <ashley.byrom@tesco.com>
2020-06-12 11:52:02 +0100

    50% coverage

--- index.test.js
+++ index.test.js
@@ -3,6 +3,5 @@ const index = require('./index.js');
 describe('doSomething', () => {
   it('covers both branches', () => {
     expect(index.doSomething(true)).toBe('bool is true');
-    expect(index.doSomething(false)).toBe('bool is false');
   })
 })
